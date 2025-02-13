6b59200e781a8ed01bb43833b9733ccc54647f0a
raddevus <1radeutsch1@gmail.com>
2025-02-13 08:35:30 -0500

    Number input used for speed works to make the character move more pixels each time the arrow keys are pressed & makes the character move faster / slower.

--- chpt003/js/main-005.js
+++ chpt003/js/main-005.js
@@ -11,10 +11,22 @@ var gameArea = null;
 var mouseLoc = null;
 const mainCanvasId = "gameArea";
 var boxLoc = new Point(0,0);
+var speed;
+
 function initApp(){
     
     var context = null;
     gameArea = document.querySelector(`#${mainCanvasId}`);
+    speedEl = document.querySelector("#speed");
+    speed = speedEl.valueAsNumber;
+
+    speedEl.addEventListener("change", (e) => {
+        speed = speedEl.valueAsNumber;
+        // set focus to button so that when the arrows keys 
+        // are pressed (to move the character) they don't 
+        // change the value of the speed control.
+        document.querySelector("#snapshot").focus();
+      });
 
     var locationOutput = document.querySelector("#cursor-location");
     gameArea.addEventListener("mousemove", (e)=>{
@@ -22,23 +34,25 @@ function initApp(){
         locationOutput.textContent = `${mouseLoc.X} : ${mouseLoc.Y}`;
     });
 
+
+
     document.addEventListener("keydown", (e) => {
         console.log(e.key);
         switch (e.key){
             case "ArrowUp":{
-                boxLoc.Y -= 1;
+                boxLoc.Y -= speed;
                 break;
             }
             case "ArrowDown":{
-                boxLoc.Y += 1;
+                boxLoc.Y += speed;
                 break;
             }
             case "ArrowLeft":{
-                boxLoc.X -= 1;
+                boxLoc.X -= speed;
                 break;
             }
             case "ArrowRight":{
-                boxLoc.X += 1;
+                boxLoc.X += speed;
                 break;
             }
         }
