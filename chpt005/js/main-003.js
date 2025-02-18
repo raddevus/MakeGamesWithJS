4d13657eb8cc738f58c1f46a7a4ebcc55132c03d
raddevus <1radeutsch1@gmail.com>
2025-02-18 15:17:28 -0500

    Added generation of 100 randomly placed & colored circles & draw them to the background to give a basic perf test.  Looks good so far.

--- chpt005/js/main-003.js
+++ chpt005/js/main-003.js
@@ -49,6 +49,7 @@ function initApp(){
         mouseLoc = getMousePos(e,gameArea);
         locationOutput.textContent = `${mouseLoc.X} : ${mouseLoc.Y}`;
     });
+    generateRandomCircles();
 
     document.addEventListener("keydown", (e) => {
         console.log(`${gridLoc.X} : ${gridLoc.Y}`);
@@ -72,12 +73,29 @@ function initApp(){
             }
         }
         console.log(`${gridLoc.X} : ${gridLoc.Y}`);
+        
         ClearCanvas();
         Draw();
     });
 
     app = new App(mainCanvasId);
     Draw();
+}
+
+function generateRandomCircles(){
+    for (var i = 0; i < 100;i++){
+        
+        var x = (getRandom(40) + 1) * 25;
+        var y = (getRandom(40) + 1) * 25;
+        // gen random color
+        var r = getRandom(255);
+        var g = getRandom(255);
+        var b = getRandom(255);
+        var rndColor = `rgba(${r}, ${g}, ${b}`;
+        // gen random-ish point
+        var p = new Point(x,y)
+        allCircles.push(new Circle(p,rndColor));
+    }
 }
 
 function DrawCircle(){
@@ -91,13 +109,32 @@ function DrawCircle(){
     // Draw the circle
     app.context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
     // Set the fill color
-    app.context.fillStyle = 'blue';
+    app.context.fillStyle =  `rgba(255, 99, 71, 0.4)`;//'blue';
     // Fill the circle with the color
     app.context.fill();
     // Set the border color
     app.context.strokeStyle = 'black';
     // Draw the border
     
+}
+
+function DrawRandomCircles(){
+
+    allCircles.forEach(c => {
+     
+     // Begin a new path
+     app.context.beginPath();
+     // Draw the circle
+     app.context.arc(c.Point.X + gridLoc.X, c.Point.Y + gridLoc.Y, radius, 0, 2 * Math.PI, false);
+     // Set the fill color
+     app.context.fillStyle =  c.Color;
+     // Fill the circle with the color
+     app.context.fill();
+     // Set the border color
+     app.context.strokeStyle = 'black';
+     // Draw the border
+     app.context.stroke();
+    });
 }
 
 function ClearCanvas(){
@@ -110,6 +147,7 @@ function Draw(){
     
     app.context.drawImage(gridImage,gridLoc.X, gridLoc.Y,1000,1000);
     DrawCircle();
+    DrawRandomCircles();
     app.context.drawImage(boxImage, 230, 230,avatarWidth,avatarWidth);
 
 }
@@ -134,5 +172,12 @@ class App{
     constructor(canvasId){
         this.gameArea = document.querySelector(`#${canvasId}`);
         this.context = this.gameArea.getContext("2d");
+    }
+}
+
+class Circle{
+    constructor(point, color){
+        this.Point = point;
+        this.Color = color;
     }
 }
